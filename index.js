63015418440cc07528efc67072fbed2be6460ef5
Ezhiloli <ezhiloli07@gmail.com>
2022-10-11 18:35:44 +0530

    Adding User input and Adding delete functionality and Adding Basic Styles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 8080;
 // path is needed whenever we need set directory setup
 const path = require('path');
+// const { redirect } = require('statuses');
 
 // To get all the functionality of express 
 const app = express();
@@ -9,6 +10,11 @@ const app = express();
 app.set('view engine','ejs'); // app{view engine:'ejs'} app have many properties it will include view engine
 // setup the views folder MVC-> V-view
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+// access static files
+app.use(express.static('assets'));
+
+
 
 
 // create Local/Static Contact List
@@ -37,6 +43,42 @@ app.get('/',function(req,res){
         contact_list:contactList
     });
 })
+app.get('/test-page',function(req,res){
+    
+    return res.render('testpage');
+})
+app.post('/create-contact',function(req,res){
+
+    if(req.body.name && req.body.phone){
+
+    contactList.push({
+        name:req.body.name,
+        phone:req.body.phone
+    })
+
+   
+    console.log(req.body);
+    console.log('Name is:',req.body.name);
+    console.log('Phone Number is:',req.body.phone);
+    }
+
+    return res.redirect('back');/// return res.redirect('/') same as <= ;  
+    // return res.redirect('test-page')
+});
+
+app.get('/delete-contact/',function(req,res){
+    // 2 ways to get the data from url 1.Query Param 2. String Param
+    // req.params is object
+    console.log(req.query);
+    let phone = req.query.phone;
+
+    let contactIndex = contactList.findIndex(contact => contact.phone == phone);
+
+    if(contactIndex!=1){
+        contactList.splice(contactIndex,1);
+    }
+    return res.redirect('back')
+});
 
 app.listen(port,function(err){
     if(err){
