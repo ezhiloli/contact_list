266d0a96e7ed8c11a2d6ef3b191b4c0883413b21
Ezhiloli <ezhiloli07@gmail.com>
2022-10-11 15:03:01 +0530

    Local Contact List has been Created and Send to View Engine

--- index.js
+++ index.js
@@ -10,12 +10,32 @@ app.set('view engine','ejs'); // app{view engine:'ejs'} app have many properties
 // setup the views folder MVC-> V-view
 app.set('views',path.join(__dirname,'views'));
 
+
+// create Local/Static Contact List
+var contactList = [
+    {
+        name:'Ezhiloli',
+        phone:'9380364937'
+    },
+    {
+        name:'Stark',
+        phone:'487659264',
+    },
+    {
+        name:'Captain America',
+        phone:'8363036402'
+    }
+]
+
 // send res or add checkpoint
 app.get('/',function(req,res){
     // res.send('Cool, it is running!');
     // Everthing in JS is Obj => locals ={ title:"My Contact List"}
     // We can Access in index file like this => locals.title
-    return res.render('index',{title:"My Contacts List"});
+    return res.render('index',{
+        title:"My Contacts List",
+        contact_list:contactList
+    });
 })
 
 app.listen(port,function(err){
